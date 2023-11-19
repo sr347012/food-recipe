d736d4e3d5e2ad7815f9eace1e9c3c6d92462cff
Sriram <sriramachandra2387.k@gmail.com>
2023-11-19 12:40:52 +0530

    icon changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import RecipeList from './recipe-list';
 import RecipeDetail from './recipe-detail';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import RecipeHome from './recipe-home';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/react-food-recipe" exact component={RecipeList}></Route>
-        <Route path="/" exact component={RecipeList}></Route>
+        <Route path="/" exact component={RecipeHome}></Route>
         <Route path="/:id" component={RecipeDetail}></Route>
       </Switch>
     </BrowserRouter>
---
+++ src/recipe-home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import recipeHome from './recipe-home.module.css';
+
+function RecipeHome() {
+  return (
+      <div className={recipeHome.homeContainer }>
+          recipe-home
+          <h1>Recipe Home</h1>
+      </div>
+  )
+}
+
+export default RecipeHome
